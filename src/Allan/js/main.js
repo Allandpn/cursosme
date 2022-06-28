6f6862af10f898259e07a82ff00857fa2a56d24f
Allandpn <99135710+Allandpn@users.noreply.github.com>
2022-06-28 19:13:05 -0300

    Update main.js

--- src/Allan/js/main.js
+++ src/Allan/js/main.js
@@ -16,9 +16,9 @@ function init(){
 
   _token = '11748~KMkhyKZmmpxczIIaViCererBxWNW4uWPC1wOxhsgelK5RPcpHSRjsnvgMs3BvdgL'
 
-  fetch('https://pucminas.instructure.com/api/v1/users/self', {
-    'method': 'GET',
-    'headers': {
+  fetch('https://pucminas.instructure.com/api/v1/courses/87896', {
+    method: 'GET',
+    headers: {
         'Authorization': 'Bearer 11748~KMkhyKZmmpxczIIaViCererBxWNW4uWPC1wOxhsgelK5RPcpHSRjsnvgMs3BvdgL',
        // 
     }
