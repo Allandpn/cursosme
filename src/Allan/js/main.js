29b185e2bad8e23babd7a22fbf901c4e6bc5102b
Allandpn <99135710+Allandpn@users.noreply.github.com>
2022-07-02 11:16:23 -0300

    a

--- src/Allan/js/main.js
+++ src/Allan/js/main.js
@@ -3,26 +3,6 @@
 function init(){
 
 
-  var db_assigments = "" 
-  const token = "11748~KMkhyKZmmpxczIIaViCererBxWNW4uWPC1wOxhsgelK5RPcpHSRjsnvgMs3BvdgL"
-  db_assigments = fetch('https://pucminas.instructure.com/api/v1/courses/87896/assignments?per_page=2000',
-            {
-                method: "get",
-                headers: {
-                  //  'Content-Type' : 'application/json',
-                  //  'Content-Type' : 'application/json',
-                  //  'Access-Control-Allow-Origin' : '*',
-                  //  'Access-Control-Allow-Headers' : '*',
-                  //  'Access-Control-Allow-Credentials' :  'true',
-                    "Authorization": `Bearer ${token}`
-                },
-            })
-            .then((db_assigments) => db_assigments.json())
-            .then((data) => {
-               // db_assigments = data;
-                console.log(data)
-            })
-            .catch((error) => console.log(error.message))
 
 
   //Carrega a lista de atributos
@@ -44,6 +24,31 @@ function init(){
 
 */
 
+function getData() {
+
+  var db_assigments = "" 
+  const token = "11748~KMkhyKZmmpxczIIaViCererBxWNW4uWPC1wOxhsgelK5RPcpHSRjsnvgMs3BvdgL"
+  db_assigments = fetch('https://pucminas.instructure.com/api/v1/courses/87896/assignments?per_page=2000',
+      {
+          method: "get",
+          headers: {
+            //  'Content-Type' : 'application/json',
+            //  'Content-Type' : 'application/json',
+            //  'Access-Control-Allow-Origin' : '*',
+            //  'Access-Control-Allow-Headers' : '*',
+            //  'Access-Control-Allow-Credentials' :  'true',
+              "Authorization": `Bearer ${token}`
+          },
+      })
+      .then((db_assigments) => db_assigments.json())
+      .then((data) => {
+         // db_assigments = data;
+          console.log(data)
+      })
+      .catch((error) => console.log(error.message))
+
+
+} 
 
 
 
